d830c04e70a63dfee3bca19965dc06d2e6872b4f
saache99 <91719995+saache99@users.noreply.github.com>
2021-10-08 12:38:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+function draw()
+{
+ 
+}
+function save_it()
+{
+    save('myFilterImage.png');
+}
